c9290c037fd7bb152703656877e817955fdc52cc
brinews <brinewsor@gmail.com>
2015-05-01 23:18:46 +0800

    C Redis Simple

---
+++ C/smb-javahash/C-DICT/src/snapshot.h
@@ -0,0 +1,31 @@
+#ifndef SNAPSHOT_H
+#define SNAPSHOT_H
+
+#define MAX_KEY_LENGTH 16
+#define MAX_LINE_LENGTH 1024
+
+typedef struct value value;
+typedef struct entry entry;
+typedef struct snapshot snapshot;
+
+struct value {
+    value * prev;
+    value * next;
+    int value;
+};
+
+struct entry {
+    entry * prev;
+    entry * next;
+    value * values;
+    char key[MAX_KEY_LENGTH];
+};
+
+struct snapshot {
+    snapshot * prev;
+    snapshot * next;
+    entry * entries;
+    int id;
+};
+#endif
+
